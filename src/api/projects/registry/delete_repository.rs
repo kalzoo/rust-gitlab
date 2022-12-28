3c5ea3a226ad4c81d6952a673b9a3369824db8ff
Ben Boeckel <ben.boeckel@kitware.com>
2022-12-28 14:51:17 -0500

    api/registry: derive `Clone` for new endpoints

--- src/api/projects/registry/delete_repository.rs
+++ src/api/projects/registry/delete_repository.rs
@@ -10,7 +10,7 @@ use crate::api::common::NameOrId;
 use crate::api::endpoint_prelude::*;
 
 /// Delete a registry repository within a project.
-#[derive(Debug, Builder)]
+#[derive(Debug, Builder, Clone)]
 pub struct DeleteRepository<'a> {
     /// The project to delete from.
     #[builder(setter(into))]
